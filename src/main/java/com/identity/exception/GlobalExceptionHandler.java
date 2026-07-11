57846ce8d9ff23cd184448ddf0a2b23987ca22ae
Abdul Rasheed Momand <abrashee89@gmail.com>
2026-07-11 23:50:29 +0200

    security: log unhandled identity exceptions

--- src/main/java/com/identity/exception/GlobalExceptionHandler.java
+++ src/main/java/com/identity/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.identity.exception;
 
 import com.identity.dto.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,9 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<ApiResponse<Void>> handleAuth(AuthException ex) {
@@ -46,6 +51,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGeneric(Exception ex) {
+        log.error("Unhandled exception", ex);
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ApiResponse<>("An unexpected error occurred", null));
     }
